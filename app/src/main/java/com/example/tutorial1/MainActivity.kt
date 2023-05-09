5a9a9b2f75b49ed571eae23f947cea514ef0872d
Patryk W <patrykwiktorski@onet.pl>
2023-05-09 13:29:12 +0200

    dodano ramki

--- app/src/main/java/com/example/tutorial1/MainActivity.kt
+++ app/src/main/java/com/example/tutorial1/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -43,6 +47,7 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier
                     .size(40.dp)
                     .clip(CircleShape)
+                    .border(2.dp, MaterialTheme.colors.primary, CircleShape)
             )
 
             Spacer(modifier = Modifier.width(8.dp))
