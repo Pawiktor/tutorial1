bff15a28ceecdbece09bc824cfedb70f7d5ce80a
Patryk W <patrykwiktorski@onet.pl>
2023-05-09 13:02:51 +0200

    dodano ikone

--- app/src/main/java/com/example/tutorial1/MainActivity.kt
+++ app/src/main/java/com/example/tutorial1/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.tutorial1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tutorial1.ui.theme.Tutorial1Theme
 
@@ -25,6 +28,12 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MessageCard(msg: Message) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "test icon"
+            )
+        }
         Column {
             Text(text = "Hello ${msg.author}")
             Text(text = msg.body)
