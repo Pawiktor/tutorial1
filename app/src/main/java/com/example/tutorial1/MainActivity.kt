980b8ed89ef5bb93cc9afcb56115218952cded64
Patryk W <patrykwiktorski@onet.pl>
2023-05-09 13:10:36 +0200

    dodano nowe marginesy 1

--- app/src/main/java/com/example/tutorial1/MainActivity.kt
+++ app/src/main/java/com/example/tutorial1/MainActivity.kt
@@ -7,13 +7,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tutorial1.ui.theme.Tutorial1Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,17 +33,19 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MessageCard(msg: Message) {
-        Row {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = "test icon"
+                contentDescription = "test icon",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
             )
+            Column {
+                Text(text = "Hello ${msg.author}")
+                Text(text = msg.body)
+            }
         }
-        Column {
-            Text(text = "Hello ${msg.author}")
-            Text(text = msg.body)
-        }
-
     }
 
     @Preview(showSystemUi = true)
